f1ef6de0eacd1feb6138366fdf0abaa06dced6ef
sneha <snehaparekar98@gmail.com>
2024-12-17 09:02:43 +0000

    add

---
+++ fun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function welcome {
+        echo "-----"
+        echo "welcome $1"  # $1 is the first argument passed to the function
+        echo "-----"
+}
+
+welcome sneha
+welcome shubham
